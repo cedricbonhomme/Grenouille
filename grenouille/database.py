c8cc42d27d3287b9e115a86b90d25182d708e36f
Cédric Bonhomme <cedric@cedricbonhomme.org>
2013-07-01 21:51:44 +0200

    Not going to use ElasticSearch.

--- grenouille/database.py
+++ grenouille/database.py
@@ -2,14 +2,12 @@
 #-*- coding: utf-8 -*-
 
 import json
-from pyelasticsearch import ElasticSearch
 
 
 class WeatherDatabase(object):
 
     def __init__(self, server='http://0.0.0.0:9901'):
         self.server = server
-        self.es = ElasticSearch(server)
 
     def index(self, data):
         return self.es.index('weather', 'sensor', data)

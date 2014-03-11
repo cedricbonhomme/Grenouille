967a519ffc4ba1de8ed984b656053ba1c861f270
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-03-12 00:27:49 +0100

    updated models

--- grenouille/models.py
+++ grenouille/models.py
@@ -10,6 +10,11 @@ class User(db.Model):
     email = db.Column(db.String(120), index = True, unique = True)
     stations = db.relationship('Station', backref = 'owner', lazy = 'dynamic')
     
+    def __init__(self, firstname, lastname, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+
     def __repr__(self):
         return '<User %r>' % (self.firstname)
 
@@ -19,9 +24,24 @@ class Station(db.Model):
     altitude = db.Column(db.Float())
     latitude = db.Column(db.Float())
     longitude = db.Column(db.Float())
+    mesures = db.relationship('Mesure', backref = 'station', lazy = 'dynamic')
+
+    def __init__(self, name, altitude, longitude, latitude):
+        self.name = name
+        self.altitude = altitude
+        self.longitude = longitude
+        self.latitude = latitude
+
+    def __repr__(self):
+        return '<Station %r>' % (self.name)
+    
+class Mesure(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     temperature = db.Column(db.Float())
     pression = db.Column(db.Float())
     humidity = db.Column(db.Float())
-    
-    def __repr__(self):
-        return '<Station %r>' % (self.name)
+
+    def __init__(self, temperature, pression, humidity):
+        self.temperature = temperature
+        self.pression = pression
+        self.humidity = humidity

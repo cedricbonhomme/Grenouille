fdd2a366eb38cd38b071b5443cf690d758bf69fb
Cédric Bonhomme <kimble.mandel@gmail.com>
2014-03-12 08:11:52 +0100

    Moved db_create.py

--- db_create.py
+++ db_create.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from config import SQLALCHEMY_DATABASE_URI
+from grenouille.config import SQLALCHEMY_DATABASE_URI
 from grenouille import db
 
 db.create_all()

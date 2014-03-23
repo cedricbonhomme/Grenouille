81f7763db67b7fa6f5f6e8a4649cd199561d2770
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-03-23 12:20:43 +0100

    Updated initialization script.

--- db_create.py
+++ db_create.py
@@ -61,22 +61,14 @@ db.create_all()
 role_admin = Role(name="admin")
 role_user = Role(name="user")
 
-user1 = User(firstname="admin", lastname="admin", email="admin@mail.com", pwdhash=generate_password_hash("password"))
-user2 = User(firstname="John", lastname="Doe", email="john.doe@mail.com", pwdhash=generate_password_hash("password"))
+user1 = User(firstname="admin", lastname="admin", email="root@localhost", pwdhash=generate_password_hash("root"))
 
 user1.roles.extend([role_admin, role_user])
-user2.roles.append(role_user)
 
 station1 = Station(name="Metz", country='FR', altitude=200, latitude=49.115558, longitude=6.175635, user_id=user1.id)
 station2 = Station(name="Luxembourg Kirchberg", country='LU', altitude=300, latitude=49.6286904, longitude=6.1626319, user_id=user1.id)
 station3 = Station(name="New-York", country='US', altitude=320, latitude=40.717977, longitude=-74.006015, user_id=user1.id)
 user1.stations.extend([station1, station2, station3])
 
-station4 = Station(name="Paris", country='FR', altitude=320, latitude=40.717977, longitude=-74.006015, user_id=user2.id)
-station5 = Station(name="Bruxelles", country='BE', altitude=320, latitude=40.717977, longitude=-74.006015, user_id=user2.id)
-user2.stations.extend([station4, station5])
-
-
 db.session.add(user1)
-db.session.add(user2)
 db.session.commit()

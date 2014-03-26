6efef6ccb5333625e79f4a99f32f3cb4e248aabd
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-03-26 21:36:50 +0100

    The admininstrator of the platform can now disable the API key of the user.

--- web/views.py
+++ web/views.py
@@ -307,3 +307,26 @@ def delete_user(user_id=None):
     else:
         flash('This user does not exist.', 'danger')
     return redirect(redirect_url())
+
+@app.route('/admin/enable_user/<int:user_id>/', methods=['GET'])
+@app.route('/admin/disable_user/<int:user_id>/', methods=['GET'])
+@login_required
+@admin_permission.require()
+def disable_user(user_id=None):
+    """
+    Enable or disable the API key of a user.
+    """
+    user = User.query.filter(User.id == user_id).first()
+    if user != None:
+        if user.apikey != "":
+            user.apikey = ""
+            flash('API key of the user "' + user.firstname + '" successfully disabled.', 'success')
+        else:
+            import random, base64, hashlib
+            user.apikey = base64.b64encode(hashlib.sha512( str(random.getrandbits(256)) ).digest(),
+                                                           random.choice(['rA','aZ','gQ','hH','hG','aR','DD'])).rstrip('==')
+            flash('API key of the user "' + user.firstname + '" successfully enabled.', 'success')
+        db.session.commit()
+    else:
+        flash('This user does not exist.', 'danger')
+    return redirect(redirect_url())

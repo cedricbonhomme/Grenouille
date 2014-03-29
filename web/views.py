c9e6565ca2a0d1e4a2a014c7b0c6092253222931
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-03-29 09:03:34 +0100

    License notices.

--- web/views.py
+++ web/views.py
@@ -1,6 +1,29 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Grenouille - An online service for weather data.
+# Copyright (C) 2014 Cédric Bonhomme - http://cedricbonhomme.org/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Cedric Bonhomme"
+__version__ = "$Revision: 0.3 $"
+__date__ = "$Date: 2014/03/16 $"
+__revision__ = "$Date: 2014/03/26 $"
+__copyright__ = "Copyright (c) Cedric Bonhomme"
+__license__ = "AGPLv3"
+
 import json
 from datetime import datetime
 from flask import render_template, jsonify, request, flash, session, url_for, redirect, g, current_app, make_response
